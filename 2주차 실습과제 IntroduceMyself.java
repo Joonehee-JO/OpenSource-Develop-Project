0a75015c109baeb59059cb0cf630fd2d2899cf60
Joonehee-JO <63311325+Joonehee-JO@users.noreply.github.com>
2021-12-01 00:18:32 +0900

    Create 2주차 실습과제 IntroduceMyself.java

---
+++ 2주차 실습과제 IntroduceMyself.java
@@ -0,0 +1,20 @@
+/*****************************
+
+* 프로그램명: 자기소개 프로그램
+
+* 작성자 :  2017038076 조준희
+
+* 작성일 : 2021-09-10
+
+*프로그램 설명 : 학과, 학년, 입학년도, 이름을 출력해주는 프로그램입니다.
+
+*********************************/
+
+public class IntroduceMyself {
+	public static void main(String[] args) {
+		System.out.println("학과 :    소프트웨어학과\n");
+		System.out.println("학년 :    2학년\n");
+		System.out.println("입학년도 : 2017-03-01\n");
+		System.out.println("이름 :    조준희");
+	}
+}
